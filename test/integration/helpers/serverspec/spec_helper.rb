815ed756b563a6a8d272e0c856287d3ae6da7a1e
Nate Flood <nate@famconnect.net>
2018-01-30 14:14:57 -0500

    changed amazon linux to centos not rhel

--- test/integration/helpers/serverspec/spec_helper.rb
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -2,17 +2,11 @@
 
 require 'serverspec'
 
-set :path, '/sbin:/usr/local/sbin:$PATH'
+set :path, '/bin:/sbin:/usr/local/sbin:$PATH'
 
 if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
   set :backend, :exec
 else
   set :backend, :cmd
   set :os, family: 'windows'
-end
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/bin:/sbin:/usr/sbin:/usr/bin'
-  end
 end
